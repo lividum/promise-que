ac30b992fc376a7c0a394bc236e9ecb8cbe2cf75
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2016-06-18 11:17:04 +0700

    add bluebird for testing purpose

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,9 +1,11 @@
-// use babel-register for coverage and test on raw source
+import Bluebird from 'bluebird';
 
 before(function () {
 
   global.assert = require('chai').assert;
   global.should = require('chai').should();
   global.expect = require('chai').expect;
+
+  if (typeof global.Promise === 'undefined') global.Promise = Bluebird;
 
 });
